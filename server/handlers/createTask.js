ba110c5f3648937e25c2c5d8fcc68d696c0506b2
EmiliodDev <ortizemilio.dev@gmail.com>
2024-08-01 10:18:34 -0600

    handlers added

---
+++ server/handlers/createTask.js
@@ -0,0 +1,28 @@
+const Task = require('../db/models/task');
+
+const createTask = async (req, res) => {
+    try {
+        const { title, isCompleted, description, time, date, isNotificationEnabled, repeatDays } = req.body;
+        const { userId } = req.params;
+
+        const task = new Task({
+            title,
+            isCompleted,
+            description,
+            time,
+            date,
+            isNotificationEnabled,
+            repeatDays,
+            user: userId,
+        });
+
+        await task.save();
+
+        res.status(200).json({ message: 'Task created:', task });
+    } catch (err) { 
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+};
+
+module.exports = {createTask};
